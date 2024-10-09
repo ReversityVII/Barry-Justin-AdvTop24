49d02648b6c54d3d42f6ec3e82486778d916c60e
ReversityVII <justin.c.barry@gmail.com>
2024-10-09 17:19:46 -0400

    More visual improvements + level
    Added instructions for the player
    
    Also added a small space for the player to run around in when they test out the feature.
    
    Time Taken: 45 Minutes - a lot of this was messing around so the product doesn't look like 45 mins worth of time.

--- Barry-Justin-AdvTop24-Project/Assets/Topic Prototype/Assets/Scripts/CameraLightIndication.cs
+++ Barry-Justin-AdvTop24-Project/Assets/Topic Prototype/Assets/Scripts/CameraLightIndication.cs
@@ -7,14 +7,18 @@ public class CameraLightIndication : MonoBehaviour
     public Material lightOn;
     public Material lightOff;
 
+    public GameObject areaLight;
+
     public void UpdateState(int state)
     {
         if (state > 0)
         {
+            areaLight.SetActive(true);
             gameObject.GetComponent<MeshRenderer>().material = lightOn;
         }
         else
         {
+            areaLight.SetActive(false);
             gameObject.GetComponent<MeshRenderer>().material = lightOff;
         }
     }
